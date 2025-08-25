71d88adecf0e5c3476b7f5bfcbace7f127f615f8
mefftranny <vivian@meth.house>
2025-08-25 11:25:37 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,33 +10,26 @@ import { buildImRouter } from './routes/im.js';
 import swaggerUi from 'swagger-ui-express';
 import { openapi } from './openapi.js';
 
-
 const app = express();
 app.disable('x-powered-by');
 app.use(cors());
 app.use(express.json({ limit: '2mb' }));
 app.use(logger);
 
-
-// OpenAPI + Swagger UI
 app.get('/openapi.json', (_req, res) => res.json(openapi));
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapi as any));
 
-
-// Routes
 app.use('/api', buildSystemRouter(() => PUBLIC_BASE_URL || ''));
 app.use('/api', buildFileRouter());
 app.use('/api', buildShellRouter());
 app.use('/api', buildChainRouter());
 app.use('/api', buildImRouter());
 
-
 app.get('/', (_req, res) => {
-res.type('text/plain').send('pulsechain-369-terminal-server: see /docs for API.');
+  res.type('text/plain').send('pulsechain-369-terminal-server: see /docs for API.');
 });
 
-
 app.listen(PORT, HOST, () => {
-// eslint-disable-next-line no-console
-console.log(`Server listening on http://${HOST}:${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`Server listening on http://${HOST}:${PORT}`);
 });
